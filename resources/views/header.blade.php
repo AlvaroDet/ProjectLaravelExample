56aadccd9a0fba26b693be67ed6bda837ce790cf
AlvaroDet <alvarodetrinidad34@gmail.com>
2022-05-19 23:42:03 -0600

    final commit, the project i'ts complete

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,44 +1,50 @@
-<?php
+<?php 
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
-
+$total=0;
+if(Session::has('user'))
+{
+  $total= ProductController::cartItem();
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Laravel Shop</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-
 <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Online Shop</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Orders</a></li>
-    </ul>
-    <form class="navbar-form navbar-left" action="/action_page.php">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search" name="search">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">Laravel Comerce</a>
       </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-    <ul class ="nav navbar-nav navbar-right">
-        <li><a href="#">Cart({{$total}})</a></li>
-</ul>
-  </div>
-</nav>
-
-<div class="container">
-</div>
-
-</body>
-</html>
+  
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="/myorders">Orders</a></li>
+        </ul>
+        <form action="/search" class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" name="query" class="form-control search-box" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Search</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/cartlist">cart({{$total}})</a></li>
+          @if(Session::has('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li><a href="/login">Login</a></li>
+          @endif
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,9 +3,13 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form action="login" method="POST" >
+            <form action="register" method="POST" >
+            @csrf
+            <div class="form-group">
+                    <label for="exampleInputEmail1">Username</label>
+                    <input type="text" name="Full name" class="form-control" id="exampleInputEmail1" placeholder="Username">
+                    </div>
                 <div class="form-group">
-                    @csrf
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
                 </div>
